767e51196b87665142a83771c64dffdabc8d0500
Keziah Biermann <development@kizzycode.de>
2022-03-20 17:02:46 +0100

    Start `java` with `exec` to pass signals

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ set -eu
 
 # Go into minecraft dir
 cd "/var/minecraft"
-java -Xmx8192M -Xms2048M -jar "/usr/libexec/minecraft-server.jar" nogui
+exec java -Xmx8192M -Xms2048M -jar "/usr/libexec/minecraft-server.jar" nogui
